e10b6f33804b872de25b76b06d3ef5ceeb0b31a2
IliaAnastassov <ilia.anastassov@gmail.com>
2017-01-20 17:55:10 +0200

    Initial commit

---
+++ ProgrammingInCSharp/Chapter1/Program.cs
@@ -0,0 +1,52 @@
+﻿namespace Chapter1
+{
+    using System;
+    using System.Threading;
+
+    public class Program
+    {
+        public static void ThreadMethod()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine($"ThreadProc: {i}");
+                Thread.Sleep(0);
+            }
+        }
+
+        public static void ThreadMethod(object count)
+        {
+            for (int i = 0; i < (int)count; i++)
+            {
+                Console.WriteLine($"Parameterized ThreadProc: {i}");
+                Thread.Sleep(0);
+            }
+        }
+
+        [ThreadStatic]
+        public static int counter;
+
+        public static void Main(string[] args)
+        {
+            new Thread(() =>
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    counter++;
+                    Console.WriteLine($"Thread A: {counter}");
+                    Thread.Sleep(100);
+                }
+            }).Start();
+
+            new Thread(() =>
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    counter++;
+                    Console.WriteLine($"Thread B: {counter}");
+                    Thread.Sleep(100);
+                }
+            }).Start();
+        }
+    }
+}
