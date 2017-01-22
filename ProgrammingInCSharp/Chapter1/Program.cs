15bec521bbc7e756c92a989ff957f696750e42ae
Ilia Anastassov <ilia.anastassov@gmail.com>
2017-01-22 23:39:25 +0200

    Update Chapter1

--- ProgrammingInCSharp/Chapter1/Program.cs
+++ ProgrammingInCSharp/Chapter1/Program.cs
@@ -5,28 +5,38 @@
 
     public class Program
     {
-        public static void ThreadMethod()
-        {
-            for (int i = 0; i < 10; i++)
-            {
-                Console.WriteLine($"ThreadProc: {i}");
-                Thread.Sleep(0);
-            }
-        }
-
-        public static void ThreadMethod(object count)
-        {
-            for (int i = 0; i < (int)count; i++)
-            {
-                Console.WriteLine($"Parameterized ThreadProc: {i}");
-                Thread.Sleep(0);
-            }
-        }
-
         [ThreadStatic]
-        public static int counter;
+        private static int counter;
+        private static ThreadLocal<int> managedThreadId = new ThreadLocal<int>(() =>
+        {
+            return Thread.CurrentThread.ManagedThreadId;
+        });
 
         public static void Main(string[] args)
+        {
+
+        }
+
+        private static void UseThreadLocal()
+        {
+            new Thread(() =>
+            {
+                for (int i = 0; i < managedThreadId.Value; i++)
+                {
+                    Console.WriteLine($"Thread A: {i}");
+                }
+            }).Start();
+
+            new Thread(() =>
+            {
+                for (int i = 0; i < managedThreadId.Value; i++)
+                {
+                    Console.WriteLine($"Thread B: {i}");
+                }
+            }).Start();
+        }
+
+        private static void UseThreadStaticField()
         {
             new Thread(() =>
             {
@@ -47,6 +57,24 @@
                     Thread.Sleep(100);
                 }
             }).Start();
+        }
+
+        public static void ThreadMethod()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine($"ThreadProc: {i}");
+                Thread.Sleep(0);
+            }
+        }
+
+        public static void ThreadMethod(object count)
+        {
+            for (int i = 0; i < (int)count; i++)
+            {
+                Console.WriteLine($"Parameterized ThreadProc: {i}");
+                Thread.Sleep(0);
+            }
         }
     }
 }
