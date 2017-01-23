995287fb0af73c1dc285f57edb38b37c3045feaf
IliaAnastassov <ilia.anastassov@gmail.com>
2017-01-23 18:51:55 +0200

    Update Chapter 1

--- ProgrammingInCSharp/Chapter1/Program.cs
+++ ProgrammingInCSharp/Chapter1/Program.cs
@@ -1,7 +1,9 @@
 ﻿namespace Chapter1
 {
     using System;
+    using System.Linq;
     using System.Threading;
+    using System.Threading.Tasks;
 
     public class Program
     {
@@ -14,10 +16,173 @@
 
         public static void Main(string[] args)
         {
+            Task<int>[] tasks = new Task<int>[3];
 
+            tasks[0] = Task.Run(() =>
+            {
+                Thread.Sleep(2000);
+                return 1;
+            });
+
+            tasks[1] = Task.Run(() =>
+            {
+                Thread.Sleep(1000);
+                return 2;
+            });
+
+            tasks[2] = Task.Run(() =>
+            {
+                Thread.Sleep(3000);
+                return 3;
+            });
+
+            while (tasks.Length > 0)
+            {
+                var i = Task.WaitAny(tasks);
+                var task = tasks[i];
+
+                Console.WriteLine(task.Result);
+
+                var temp = tasks.ToList();
+                temp.RemoveAt(i);
+                tasks = temp.ToArray();
+            }
         }
 
-        private static void UseThreadLocal()
+        private static void UseTaskWaitAll()
+        {
+            var tasks = new Task[3];
+
+            tasks[0] = Task.Run(() =>
+            {
+                Thread.Sleep(1000);
+                Console.WriteLine("1");
+            });
+
+            tasks[1] = Task.Run(() =>
+            {
+                Thread.Sleep(1000);
+                Console.WriteLine("2");
+            });
+
+            tasks[2] = Task.Run(() =>
+            {
+                Thread.Sleep(1000);
+                Console.WriteLine("3");
+            });
+
+            Task.WaitAll(tasks);
+        }
+
+        private static void UseTaskFactory()
+        {
+            Task<int[]> parent = Task.Run(() =>
+            {
+                var result = new int[3];
+
+                var tf = new TaskFactory(TaskCreationOptions.AttachedToParent, TaskContinuationOptions.ExecuteSynchronously);
+
+                tf.StartNew(() => result[0] = 0);
+                tf.StartNew(() => result[1] = 1);
+                tf.StartNew(() => result[2] = 2);
+
+                return result;
+            });
+
+            var finalTask = parent.ContinueWith(p =>
+            {
+                foreach (var number in p.Result)
+                {
+                    Console.WriteLine(number);
+                }
+            });
+
+            finalTask.Wait();
+        }
+
+        private static void UseTaskWithChildTasks()
+        {
+            Task<int[]> parent = Task.Run(() =>
+            {
+                var results = new int[3];
+                new Task(() => results[0] = 0, TaskCreationOptions.AttachedToParent).Start();
+                new Task(() => results[1] = 1, TaskCreationOptions.AttachedToParent).Start();
+                new Task(() => results[2] = 2, TaskCreationOptions.AttachedToParent).Start();
+
+                return results;
+            });
+
+            var finalTask = parent.ContinueWith(parentTask =>
+            {
+                foreach (var number in parentTask.Result)
+                {
+                    Console.WriteLine(number);
+                }
+            });
+
+            finalTask.Wait();
+        }
+
+        private static void UseTaskWithContinuation()
+        {
+            Task<int> task = Task.Run(() =>
+            {
+                return 66;
+            });
+
+            task.ContinueWith((t) =>
+            {
+                Console.WriteLine("Canceled");
+            }, TaskContinuationOptions.OnlyOnCanceled);
+
+            task.ContinueWith((t) =>
+            {
+                Console.WriteLine("Faulted");
+            }, TaskContinuationOptions.OnlyOnFaulted);
+
+            var completedTask = task.ContinueWith((t) =>
+            {
+                Console.WriteLine("Completed");
+            }, TaskContinuationOptions.OnlyOnRanToCompletion);
+
+            completedTask.Wait();
+        }
+
+        private static void StartNewTask()
+        {
+            var t = Task.Run(() =>
+            {
+                for (int i = 0; i < 50; i++)
+                {
+                    Console.Write("*");
+                }
+            });
+
+            var t2 = Task.Run(() =>
+            {
+                for (int i = 0; i < 50; i++)
+                {
+                    Console.Write("+");
+                }
+            });
+
+            t.Wait();
+            t2.Wait();
+
+            Console.WriteLine();
+        }
+
+        private static void QueueWorkToThreadPool()
+        {
+            ThreadPool.QueueUserWorkItem((s) =>
+            {
+                Console.WriteLine("Working on a thread from the thread pool");
+            });
+
+            Console.ReadLine();
+        }
+
+        private static void UseThreadLocalAttribute()
         {
             new Thread(() =>
             {
