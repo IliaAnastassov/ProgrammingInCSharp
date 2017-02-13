68cd28a2714fb81b787d285f2f840f25858c8327
IliaAnastassov <ilia.anastassov@gmail.com>
2017-02-13 18:52:32 +0200

    Update Chapter1

--- ProgrammingInCSharp/Chapter1_Objective5/Program.cs
+++ ProgrammingInCSharp/Chapter1_Objective5/Program.cs
@@ -1,12 +1,112 @@
 ﻿namespace Chapter1_Objective5
 {
     using System;
+    using System.IO;
+    using System.Runtime.ExceptionServices;
 
     class Program
     {
         static void Main(string[] args)
         {
 
+        }
+
+        private static void UseExceptionDispatchInfo()
+        {
+            ExceptionDispatchInfo possibleException = null;
+
+            try
+            {
+                var number = int.Parse(Console.ReadLine());
+            }
+            catch (FormatException ex)
+            {
+                possibleException = ExceptionDispatchInfo.Capture(ex);
+            }
+
+            if (possibleException != null)
+            {
+                possibleException.Throw();
+            }
+        }
+
+        private static void UsePassInnerException()
+        {
+            try
+            {
+                PassInnerException();
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine(ex.InnerException.Message);
+            }
+        }
+
+        private static void PassInnerException()
+        {
+            var input = Console.ReadLine();
+
+            try
+            {
+                var number = int.Parse(input);
+            }
+            catch (FormatException ex)
+            {
+                throw new ArgumentException("Please use a valid number.", ex);
+            }
+            catch (OverflowException ex)
+            {
+                var message = GetOverflowExceptionMessage(input);
+
+                throw new ArgumentException(message, ex);
+            }
+        }
+
+        private static string GetOverflowExceptionMessage(string input)
+        {
+            string message;
+
+            if (input.StartsWith("-"))
+            {
+                message = "Please use a larger number.";
+            }
+            else
+            {
+                message = "Please use a smaller number.";
+            }
+
+            return message;
+        }
+
+        private static string OpenAndParse(string filename)
+        {
+            if (string.IsNullOrWhiteSpace(filename))
+            {
+                throw new ArgumentNullException("fileName", "Filename is required");
+            }
+
+            return File.ReadAllText(filename);
+        }
+
+        private static void InspectException()
+        {
+            try
+            {
+                var n = int.Parse(Console.ReadLine());
+                Console.WriteLine($"Parsed: {n}");
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine($"Message: {ex.Message}");
+                Console.WriteLine($"Data: {ex.Data}");
+                Console.WriteLine($"HResult: {ex.HResult}");
+                Console.WriteLine($"StackTrace: {ex.StackTrace}");
+                Console.WriteLine($"HelpLink: {ex.HelpLink}");
+                Console.WriteLine($"InnerException: {ex.InnerException}");
+                Console.WriteLine($"TargetSite: {ex.TargetSite}");
+                Console.WriteLine($"Source: {ex.Source}");
+            }
         }
 
         private static void UseEnvironmentFailFast()
