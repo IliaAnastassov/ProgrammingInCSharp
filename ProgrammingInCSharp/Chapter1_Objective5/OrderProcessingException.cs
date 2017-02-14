cd9eb46d7cc02ff121596ce77fb2ea9cc6e48677
IliaAnastassov <ilia.anastassov@gmail.com>
2017-02-14 18:35:25 +0200

    Update Chapter1

---
+++ ProgrammingInCSharp/Chapter1_Objective5/OrderProcessingException.cs
@@ -0,0 +1,34 @@
+﻿namespace Chapter1_Objective5
+{
+    using System;
+    using System.Runtime.Serialization;
+
+    [Serializable]
+    public class OrderProcessingException : Exception, ISerializable
+    {
+        public OrderProcessingException(int orderId)
+        {
+            OrderId = orderId;
+            HelpLink = "http://www.mydomain.com/OrderProcessingExceptionInfo";
+        }
+
+        public OrderProcessingException(int orderId, string message) : base(message)
+        {
+            OrderId = orderId;
+            HelpLink = "http://www.mydomain.com/OrderProcessingExceptionInfo";
+        }
+
+        public OrderProcessingException(int orderId, string message, Exception innerExcepion) : base(message, innerExcepion)
+        {
+            OrderId = orderId;
+            HelpLink = "http://www.mydomain.com/OrderProcessingExceptionInfo";
+        }
+
+        protected OrderProcessingException(SerializationInfo info, StreamingContext context)
+        {
+            OrderId = (int)info.GetValue("OrderId", typeof(int));
+        }
+
+        public int OrderId { get; set; }
+    }
+}
