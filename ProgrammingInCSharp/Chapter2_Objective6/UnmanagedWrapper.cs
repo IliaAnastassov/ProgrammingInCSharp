dc7ec63a5d4337970d0be7ec9ae583760ac5de0b
Ilia Anastassov <ilia.anastassov@gmail.com>
2017-03-20 22:49:14 +0200

    Update Chpater2

---
+++ ProgrammingInCSharp/Chapter2_Objective6/UnmanagedWrapper.cs
@@ -0,0 +1,42 @@
+﻿namespace Chapter2_Objective6
+{
+    using System;
+    using System.IO;
+
+    public class UnmanagedWrapper : IDisposable
+    {
+        public UnmanagedWrapper()
+        {
+            Stream = File.Open("temp.dat", FileMode.Create);
+        }
+
+        ~UnmanagedWrapper()
+        {
+            Dispose(false);
+        }
+
+        public FileStream Stream { get; private set; }
+
+        public void Close()
+        {
+            Dispose();
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        public void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                if (Stream != null)
+                {
+                    Stream.Close();
+                }
+            }
+        }
+    }
+}
