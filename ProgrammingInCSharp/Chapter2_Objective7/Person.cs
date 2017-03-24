065153954c092d15bffe224bc2d15aed5c5a1db4
Ilia Anastassov <ilia.anastassov@gmail.com>
2017-03-24 14:54:52 +0200

    Update Chapter2

---
+++ ProgrammingInCSharp/Chapter2_Objective7/Person.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Chapter2_Objective7
+{
+    public class Person
+    {
+        public Person(string firstName, string lastName)
+        {
+            this.FirstName = firstName;
+            this.LastName = lastName;
+        }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string ToString(string format)
+        {
+            string result;
+
+            if (string.IsNullOrWhiteSpace(format) || format == "G")
+            {
+                format = "FL";
+            }
+
+            format = format.Trim()
+                           .ToUpperInvariant();
+
+            switch (format)
+            {
+                case "FL":
+                    result = $"{FirstName} {LastName}";
+                    break;
+                case "LF":
+                    result = $"{LastName} {FirstName}";
+                    break;
+                case "FSL":
+                    result = $"{FirstName}, {LastName}";
+                    break;
+                case "LSF":
+                    result = $"{LastName}, {FirstName}";
+                    break;
+                default:
+                    throw new FormatException($"The {format} format string is not supported");
+            }
+
+            return result;
+        }
+    }
+}
---
+++ ProgrammingInCSharp/Chapter2_Objective7/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Chapter2_Objective7
+{
+    using System;
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+        }
+
+        private static void UsePersonFormatting()
+        {
+            var man = new Person("Dildo", "Schwaggins");
+            Console.WriteLine(man);
+            Console.WriteLine(man.ToString());
+            Console.WriteLine(man.ToString(null));
+            Console.WriteLine(man.ToString("fl"));
+            Console.WriteLine(man.ToString("lf"));
+            Console.WriteLine(man.ToString("fsl"));
+            Console.WriteLine(man.ToString("lsf"));
+        }
+    }
+}
