40495871eda1f8f904fbb07f48843d968b874627
Ilia Anastassov <ilia.anastassov@gmail.com>
2017-03-28 00:19:50 +0300

    Update Chpater3

--- ProgrammingInCSharp/Chapter3_Objective1/Program.cs
+++ ProgrammingInCSharp/Chapter3_Objective1/Program.cs
@@ -1,6 +1,8 @@
 ﻿namespace Chapter3_Objective1
 {
     using System;
+    using System.Globalization;
+    using System.Text.RegularExpressions;
     using DataAccess;
     using Models;
     using Validation;
@@ -9,7 +11,62 @@
     {
         static void Main(string[] args)
         {
-            ValidateAddress();
+
+        }
+
+        private static bool IsJson(string input)
+        {
+            input = input.Trim();
+            return input.StartsWith("{") && input.EndsWith("}") ||
+                   input.StartsWith("[") && input.EndsWith("]");
+        }
+
+        private static void RemoveExcessWhitespace()
+        {
+            var regex = new Regex(@"[ ]{2,}", RegexOptions.None);
+
+            var input = "A B  C   D";
+            var result = regex.Replace(input, " ");
+
+            Console.WriteLine(result);
+        }
+
+        private static bool ValidateZipCode(string zipCode)
+        {
+            Match match = Regex.Match(zipCode, @"^[1-9][0-9]{3}\s?[a-zA-Z]{2}$", RegexOptions.IgnoreCase);
+            return match.Success;
+        }
+
+        private static void UseConvertWithNull()
+        {
+            var i = Convert.ToInt32(null);
+            Console.WriteLine(i);
+        }
+
+        private static void ParseCurrency()
+        {
+            var english = new CultureInfo("En");
+            var dutch = new CultureInfo("Nl");
+
+            var value = "19,95€";
+            var number = decimal.Parse(value, NumberStyles.Currency, dutch);
+            Console.WriteLine(number.ToString(english));
+        }
+
+        private static void UseTryParse()
+        {
+            var value = Console.ReadLine();
+            int result;
+            var success = int.TryParse(value, out result);
+
+            if (success)
+            {
+                Console.WriteLine("The value is an integer");
+            }
+            else
+            {
+                Console.WriteLine("The value is not an integer");
+            }
         }
 
         private static void ValidateAddress()
