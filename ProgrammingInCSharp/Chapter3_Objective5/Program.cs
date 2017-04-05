5fab73a4ff199f845690a9c707b0708b71f058a4
Ilia Anastassov <ilia.anastassov@gmail.com>
2017-04-05 23:01:20 +0300

    Add Chapter4

---
+++ ProgrammingInCSharp/Chapter3_Objective5/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Chapter3_Objective5
+{
+    using System.Diagnostics;
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+
+        }
+
+        private static void UseTraceSourceClass()
+        {
+            var traceSource = new TraceSource("MyTraceSource", SourceLevels.All);
+            traceSource.TraceInformation("Tracing application...");
+            traceSource.TraceEvent(TraceEventType.Critical, 0, "Critical trace");
+            traceSource.TraceData(TraceEventType.Information, 1, new object[] { "a", "b", "c" });
+
+            traceSource.Flush();
+            traceSource.Close();
+        }
+
+        private static void UseDebugClass()
+        {
+            Debug.WriteLine("Starting Application");
+            var number = 1;
+            number++;
+            Debug.Assert(number == 3);
+            Debug.WriteLineIf(number > 0, "number is greater than 0");
+        }
+    }
+}
