67b550d2085cb6c3e5a5bf26fb52cc664e4edf46
Ilia Anastassov <ilia.anastassov@gmail.com>
2017-06-10 19:27:19 +0300

    Update Chapter4

--- ProgrammingInCSharp/Chapter4_Objective2/Program.cs
+++ ProgrammingInCSharp/Chapter4_Objective2/Program.cs
@@ -14,7 +14,6 @@
     {
         public static void Main(string[] args)
         {
-
         }
 
         private static void UseXPathQUery()
---
+++ ProgrammingInCSharp/Chapter4_Objective3/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace Chapter4_Objective3
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+        }
+    }
+}
