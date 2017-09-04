563bec805a64c2654e0a59d8d4256ab20b8d3039
Ilia Anastassov <ilia.anastassov@gmail.com>
2017-09-04 21:28:59 +0300

    Update Chapter4

---
+++ ProgrammingInCSharp/Chapter4_Objective5/Person.cs
@@ -0,0 +1,9 @@
+﻿namespace Chapter4_Objective5
+{
+    class Person
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+    }
+}
--- ProgrammingInCSharp/Chapter4_Objective5/Program.cs
+++ ProgrammingInCSharp/Chapter4_Objective5/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Chapter4_Objective5
 {
@@ -7,6 +9,104 @@ namespace Chapter4_Objective5
         static void Main(string[] args)
         {
 
+        }
+
+        private static void UseStack()
+        {
+            var myStack = new Stack<string>();
+            myStack.Push("Shmuley");
+            myStack.Push("Fucking");
+            myStack.Push("Boteach");
+            myStack.Push("Does");
+            myStack.Push("!!!");
+
+            while (myStack.Count > 0)
+            {
+                var item = myStack.Pop();
+                Console.Write($"{item} ");
+            }
+
+            Console.WriteLine();
+        }
+
+        private static void UseQueue()
+        {
+            var myQueue = new Queue<string>();
+            myQueue.Enqueue("Shmuley");
+            myQueue.Enqueue("Fucking");
+            myQueue.Enqueue("Boteach");
+            myQueue.Enqueue("Does");
+            myQueue.Enqueue("!!!");
+
+            while (myQueue.Count > 0)
+            {
+                var item = myQueue.Dequeue();
+                Console.Write($"{item} ");
+            }
+
+            Console.WriteLine();
+        }
+
+        private static void UseHashSet()
+        {
+            var evenSet = new HashSet<int>();
+            var oddSet = new HashSet<int>();
+
+            for (int i = 1; i <= 20; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    evenSet.Add(i);
+                }
+                else
+                {
+                    oddSet.Add(i);
+                }
+            }
+
+            DisplayCollection(evenSet);
+            DisplayCollection(oddSet);
+
+            evenSet.UnionWith(oddSet);
+            var ordered = evenSet.OrderBy(x => x);
+            DisplayCollection(ordered);
+        }
+
+        private static void DisplayCollection(IEnumerable<int> collection)
+        {
+            Console.Write("{");
+            foreach (var item in collection)
+            {
+                Console.Write($" {item}");
+            }
+            Console.WriteLine(" }");
+        }
+
+        private static void UseDictionary()
+        {
+            var shmuley = new Person { Id = 1, Name = "Shmuley Boteach" };
+            var dildo = new Person { Id = 2, Name = "Dildo Schwaggins" };
+            var pena = new Person { Id = 3, Name = "Pena Gesheva" };
+
+            var persons = new Dictionary<int, Person>();
+            persons.Add(shmuley.Id, shmuley);
+            persons.Add(dildo.Id, dildo);
+            persons.Add(pena.Id, pena);
+
+            foreach (var pair in persons)
+            {
+                Console.WriteLine($"{pair.Key} : {pair.Value.Name}");
+            }
+
+            persons[0] = new Person { Id = 4, Name = "Jane Doe" };
+
+            Console.WriteLine(persons[0].Name);
+
+            Person person;
+            if (!persons.TryGetValue(5, out person))
+            {
+                Console.WriteLine("No person with such id");
+            }
         }
 
         private static void UseJaggedArray()
